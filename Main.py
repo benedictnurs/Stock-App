7f275f0da2db2cbcf461252ca81ed5a7677c916a
benedictnurs <benedictnursalim@gmail.com>
2022-11-21 22:59:32 -0800

    Bug Fix
    -Split sidebar column

--- Main.py
+++ Main.py
@@ -7,25 +7,43 @@ import datetime as dt
 from yahoo_fin import stock_info as si
  
 
+st.set_page_config(
+    page_title = "Stock Application",
+    page_icon="📈",
+    initial_sidebar_state="collapsed",
+    )
+
 snp_500 = pd.DataFrame( si.tickers_sp500() )
 nasdaq = pd.DataFrame( si.tickers_nasdaq() )
 dow = pd.DataFrame( si.tickers_dow() )
 other = pd.DataFrame( si.tickers_other() )
 
 
-with st.sidebar:
-    start_date = st.sidebar.date_input("Start date", dt.date(2000, 1, 1))
-    end_date = st.sidebar.date_input("End date", dt.date.today())
-    st.sidebar.market_select = st.selectbox('Pick a Market',("S&P 500", "NASDAQ", "Dow Jones", "Other"))
-    if st.sidebar.market_select == "S&P 500":
+window_sidebar = st.sidebar.container() # create an empty container in the sidebar
+sub_columns = window_sidebar.columns(2) 
+
+
+start_date = sub_columns[0].date_input("Start date", dt.date(2000, 1, 1))
+end_date = sub_columns[1].date_input("End date", dt.date.today())
+market_select = window_sidebar.selectbox('Pick a Market',("S&P 500", "NASDAQ", "Dow Jones", "Other"))
+    
+
+st.markdown(f'''
+    <style>
+        section[data-testid="stSidebar"] .css-ng1t4o {{width: 10rem;}}
+        section[data-testid="stSidebar"] .css-1d391kg {{width: 10rem;}}
+    </style>
+''',unsafe_allow_html=True)
+
+if st.sidebar.market_select == "S&P 500":
         market = snp_500
-    elif st.sidebar.market_select == "NASDAQ":
+elif st.sidebar.market_select == "NASDAQ":
         market = nasdaq
-    elif st.sidebar.market_select == "Dow Jones":
+elif st.sidebar.market_select == "Dow Jones":
         market = dow
-    elif st.sidebar.market_select == "Other":
+elif st.sidebar.market_select == "Other":
         market = other
-    st.sidebar.stock_select = st.selectbox('Pick a Ticker',(market))
+stock_select = window_sidebar.selectbox('Pick a Ticker',(market))
 
 
 
@@ -43,8 +61,6 @@ color_conditions = alt.condition("datum.Open <= datum.Close",
                                  alt.value("green"),
                                  alt.value("red"))# build chart
 chart = alt.Chart(ticker_df).encode(x = "Dates")
-# set title of chart
-chart.title = "Candle-stick Chart of "+ company_name
 # set x axis label for chart
 chart.encoding.x.title = "Time"
 # construct a rule mark using mark_rule() method
@@ -52,7 +68,7 @@ rules = chart.mark_rule().encode(
     y = "Low",
     y2 = "High")
 # adjust y axis label for rules
-rules.encoding.y.title = "Price"
+rules.encoding.y.title = "Close"
 # construct bars
 bars = chart.mark_bar().encode(
     y="Open",
@@ -60,28 +76,24 @@ bars = chart.mark_bar().encode(
     color = color_conditions)
 candlestick = rules + bars
 
+
+
+
 #Creates the regular chart 
 chart = alt.Chart(ticker_df).mark_area(line={'color':'darkgreen'},color=alt.Gradient(
         gradient='linear',
         stops=[alt.GradientStop(color='white', offset=0),
                alt.GradientStop(color='darkgreen', offset=1)])).encode(x="Dates",y="Close")
-chart.title = "Chart of "+ company_name
-# display altair chart
-
-
-
-
-
-
-# display rules and bars (both based on the same basic chart
 
 
 st.header(company_name)
 #st.markdown(string_logo, unsafe_allow_html=True)
+chart_plot = st.empty()
 agree = st.checkbox('Candle Chart')
 
 if agree:
-   st.altair_chart(candlestick, use_container_width=True)
+   chart_plot.altair_chart(candlestick, use_container_width = True)
+
 else:
-   st.altair_chart(chart, use_container_width=True)
+   chart_plot.altair_chart(chart, use_container_width = True)
 
