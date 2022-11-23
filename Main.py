5a4b7a464c43c1a5de8537f1dd458ed83fafd943
benedictnurs <benedictnursalim@gmail.com>
2022-11-22 23:30:53 -0800

    Fixed Delta and Addons
    -Fixed the delta so it is the change between the first date and the last.
    -Created a revenue and earnings chart.
    -Created comments to make the code easier to read.

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import yfinance as yf
-import datetime as dt
+import datetime
 from yahoo_fin import stock_info as si
  
 
@@ -18,23 +18,16 @@ nasdaq = pd.DataFrame( si.tickers_nasdaq() )
 dow = pd.DataFrame( si.tickers_dow() )
 other = pd.DataFrame( si.tickers_other() )
 
-
+#Sidebar of the dashboard
 window_sidebar = st.sidebar.container() # create an empty container in the sidebar
 sub_columns = window_sidebar.columns(2) 
 
-
-start_date = sub_columns[0].date_input("Start", dt.date(2010, 1, 1))
-end_date = sub_columns[1].date_input("End", dt.date.today())
+#Date selections for the dashboard
+start_date = sub_columns[0].date_input("Start", datetime.date(2010, 1, 1))
+end_date = sub_columns[1].date_input("End", datetime.date.today())
 market_select = window_sidebar.selectbox('Select Market',("S&P 500", "NASDAQ", "Dow Jones", "Other"))
 
-
-st.markdown(f'''
-    <style>
-        section[data-testid="stSidebar"] .css-ng1t4o {{width: 10rem;}}
-        section[data-testid="stSidebar"] .css-1d391kg {{width: 10rem;}}
-    </style>
-''',unsafe_allow_html=True)
-
+#Market selections for the dashboard
 if market_select == "S&P 500":
         market = snp_500
 elif market_select == "NASDAQ":
@@ -44,35 +37,31 @@ elif market_select == "Dow Jones":
 elif market_select == "Other":
         market = other
 
+#Stock selection for the dashboard
 stock_select = window_sidebar.selectbox('Select Ticker',(market))
-
 
 
 #Grabs data from the API
 ticker_stock = stock_select
-ticker_data = yf.Ticker(ticker_stock) 
-company_name = ticker_data.info['longName']
-string_logo = '<img src=%s>' % ticker_data.info['logo_url']
-ticker_df = ticker_data.history(period= '1d',start = start_date, end = end_date) #get the historical prices for this ticker
+stock = yf.Ticker(ticker_stock) 
+company_name = stock.info['longName']
+string_logo = '<img src=%s>' % stock.info['logo_url']
+ticker_df = stock.history(period= '1d',start = start_date, end = end_date) #get the historical prices for this ticker
 ticker_df["Dates"] = ticker_df.index
-summary = ticker_data.info["longBusinessSummary"]
+
+#Grabs summarization data from the API
+summary = stock.info["longBusinessSummary"]
+earnings = stock.earnings.head().style.format("{:,.0f}")
 
 #Grabs prices 
 max_price = round(ticker_df["High"].max(),2)
-current_price = round(ticker_data.info['regularMarketPrice'],2)
+current_price = round(stock.info['regularMarketPrice'],2)
+starting_price = round(ticker_df["Close"].iloc[0],2)
+
 
 #Calculates change 
-def nearest_business_day(date: dt.date):
-        """
-        Takes a date and transform it to the nearest business day, 
-        static because we would like to use it without a stock object.
-        """
-        if date.weekday() == 5:
-            date = date - dt.timedelta(days=1)
-
-        if date.weekday() == 6:
-            date = date + dt.timedelta(days=1)
-
+price_change = round(current_price - starting_price,2)
+percent_change = round(((current_price - starting_price)/starting_price) * 100,2)
 
 
 
@@ -105,14 +94,15 @@ chart = alt.Chart(ticker_df).mark_area(line={'color':'darkgreen'},color=alt.Grad
         stops=[alt.GradientStop(color='white', offset=0),
                alt.GradientStop(color='darkgreen', offset=1)])).encode(x="Dates",y="Close")
 
-
+#Title of the stock
 st.header(company_name + " ("+ticker_stock+")")
 
+#The Prices and deltas of the stock
 window_page = st.container() # create an empty container in the page
-sub_page = window_page.columns(3) 
-sub_page[0].metric("Current", current_price, "-10")
-sub_page[1].metric("1 Year High", max_price, 10)
-sub_page[2].metric("Highest", max_price)
+sub_page = window_page.columns(4) 
+sub_page[0].metric("Current Price", current_price, price_change)
+sub_page[1].metric("Start Price", starting_price, f"{percent_change}%")
+sub_page[2].metric("All-Time High", max_price)
 
 
 #st.markdown(string_logo, unsafe_allow_html=True)
@@ -124,5 +114,8 @@ if agree:
 else:
    st.altair_chart(chart, use_container_width = True)
 
-
+#Summarization of the stock
 st.caption(summary)
+st.table(earnings)
+
+
