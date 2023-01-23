6d4d8dd8dc930e06c50efdeaf32ed495ca2c0e15
benedictnurs <benedictnursalim@gmail.com>
2023-01-22 23:14:24 -0800

    Added Calculations
    -Added Calculations based on dollars invested.

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import plotly.graph_objs as go
 from yahoo_fin import stock_info as si
  
 
+st.set_page_config(
+    page_title = "Stock Application",
+    page_icon="📈",
+    initial_sidebar_state="collapsed",
+    )
+
+#Sidebar of the dashboard and creates an empty container in the sidebar
+window_sidebar = st.sidebar.container() 
+
+#Date selections for the dashboard
+sub_columns = window_sidebar.columns(2) 
+start_date = sub_columns[0].date_input("Start", datetime.date(2010, 1, 1))
+end_date = sub_columns[1].date_input("End", datetime.date.today())
+
+#Market selections for the dashboard
+market_select = window_sidebar.selectbox('Select Market',("S&P 500", "NASDAQ", "Dow Jones", "Other"))
+
+
 class Stock():
 	def __init__(self, stock):
 		self.stock = stock
@@ -32,24 +50,14 @@ class Stock():
 		return price_start
 
 
-st.set_page_config(
-    page_title = "Stock Application",
-    page_icon="📈",
-    initial_sidebar_state="collapsed",
-    )
-
-#Sidebar of the dashboard and creates an empty container in the sidebar
-window_sidebar = st.sidebar.container() 
+#To get the price change
+def price_change(current ,start):  
+	return round(current - start,2)
 
 
-#Date selections for the dashboard
-sub_columns = window_sidebar.columns(2) 
-start_date = sub_columns[0].date_input("Start", datetime.date(2010, 1, 1))
-end_date = sub_columns[1].date_input("End", datetime.date.today())
-
-
-#Market selections for the dashboard
-market_select = window_sidebar.selectbox('Select Market',("S&P 500", "NASDAQ", "Dow Jones", "Other"))
+#To get the percent change
+def percent_change(current ,start):  
+	return round(((current - start)/start) * 100,2)
 
 
 def market_selection():
@@ -67,31 +75,6 @@ def market_selection():
         elif market_select == "Other":
                 market = other
         return market
-
-
-#To get the price change
-def price_change(current ,start):  
-	return round(current - start,2)
-
-
-#To get the percent change
-def percent_change(current ,start):  
-	return round(((current - start)/start) * 100,2)
-
-
-#Stock selection for the dashboard
-stock_select = window_sidebar.selectbox('Select Ticker',(market_selection()))
-
-
-#Grabs data from the API
-stock = yf.Ticker(stock_select) 
-company_name = stock.info['longName']
-ticker_df = stock.history(period= '1d',start = start_date, end = end_date) #get the historical prices for this ticker
-ticker_df["Dates"] = ticker_df.index
-
-
-#Creates an Object of Stock
-stock_data = Stock(stock)
 
 
 #Chart Visualization
@@ -125,13 +108,23 @@ def chart():
         return fig
 
 
+#Stock selection for the dashboard
+stock_select = window_sidebar.selectbox('Select Ticker',(market_selection()))
+
+#Grabs data from the API
+stock = yf.Ticker(stock_select) 
+company_name = stock.info['longName']
+ticker_df = stock.history(period= '1d',start = start_date, end = end_date) #get the historical prices for this ticker
+ticker_df["Dates"] = ticker_df.index
+
+#Creates an Object of Stock
+stock_data = Stock(stock)
+
 #Title of the stock
 st.header(f"{company_name} ({stock_select})")
 
-
 #The prices and deltas of the stock
 window_page = st.container() # create an empty container in the page
-
 
 #The prices with the deltas
 sub_page = window_page.columns(4) 
@@ -139,16 +132,43 @@ sub_page[0].metric("Current Price", stock_data.current_price(), f"{percent_chang
 sub_page[1].metric("Start Price", stock_data.starting_price(), price_change(stock_data.current_price(),stock_data.starting_price()))
 sub_page[2].metric("All-Time High", stock_data.max_price())
 
-
 #Shows the chart 
 st.plotly_chart(chart(), use_container_width = True)
 
 #Creating tabs 
-tab1, tab2 = st.tabs(["Summarization", "Calculations"])
+tab1, tab2 = st.tabs(["Summarization", "Calculation"])
 
 
 #Summarization of the stock 
 tab1.caption(stock_data.summary())
 tab1.table(stock_data.earnings())
 
+#Tab 2 
+investedAmount = tab2.number_input("Initial Amount Invested")
 
+
+def investment_changed():
+        investedGrowth = round(percent_change(stock_data.current_price(),stock_data.starting_price())/100 * investedAmount,2)
+        investedChange = ""
+        if investedGrowth >= investedAmount:
+                investedChange = f""" <p style="font-family:sans-serif; 
+                        color:Green;">
+			${"{:,}".format(investedGrowth)}
+			</p> """
+        else:
+                investedChange = f""" <p style="font-family:sans-serif; 
+                        color:Red;">
+			${"{:,}".format(investedGrowth)}
+			</p> """
+        return investedChange
+
+
+def get_year():
+        startYear = (str(ticker_df["Dates"].iloc[0])[0:4])
+        endYear = (str(ticker_df["Dates"].iloc[-1])[0:4])
+        year = int(endYear) - int(startYear)
+        return year     
+
+
+tab2.markdown(f"Amount After {str(get_year())} Years", unsafe_allow_html=True)
+tab2.markdown(investment_changed(), unsafe_allow_html=True)
